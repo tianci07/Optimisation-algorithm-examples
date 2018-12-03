00f65ba692d0381767abb6c7084b367a151c4286
Shatha Al-Maliki <hayderal-maneea@Hayders-MacBook-Pro.local>
2018-12-03 11:58:15 +0000

    creat the swarm

--- src/PSO.py
+++ src/PSO.py
@@ -14,27 +14,33 @@ g_swarm_size  = 5
 g_Dimention   = 3
 g_repeatation = 10
 Epsilon       = 0.00005
+g_swarm       = []
 
-g_population  = PA.Particle()
+particle  = PA.Particle()
 
 
 
 # main() function
 def main():
 
-    global g_population
-
-    # Set the range for the particle
+    global g_swarm
+    global particle
+    
+    # Set the range for each particle
     boundaryset = []
     for i in range(g_Dimention):
-        boundaryset[i][0] = 0
-        boundaryset[i][1] = 10
-
-    print(boundaryset)
-    # Creat the Swarm
-    #for i in range(g_swarm_size):
+        boundaryset.append([0,10])
     
-        #g_population.Particle(g_Dimention,
+    # Creat the swarm
+    for i in range(g_swarm_size):
+    
+        particle.Particle(g_Dimention, boundaryset, 0.0, g_swarm)
+        
+        g_swarm.append(copy.deepcopy(particle.getPosition()))
+
+        particle.printing()
+
+    print("the swarm ", g_swarm)
 
 # Call the main() function to begin the program.
 if __name__ == '__main__':
