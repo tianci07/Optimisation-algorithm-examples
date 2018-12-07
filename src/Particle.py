dd7d4058b7487affbc046b65a3e5eea75abd8f13
Shatha Al-Maliki <hayderal-maneea@Hayders-MacBook-Pro.local>
2018-12-07 15:20:45 +0000

    Debug the class

--- src/Particle.py
+++ src/Particle.py
@@ -3,33 +3,30 @@ import math
 import numpy as np
 import copy
 import sys
-import PSO
 
 
 class Particle:
-    def __init__(self, aNumberOfDimentions, aBoundarySet, aCostFunction, aPSO):
+   
+    def __init__(self):
         self.position = []
         self.velocity = []
         self.cost = float('inf')
         self.best_known_position = []
         self.best_known_cost = float('inf')
         self.boundary_set = []
-    
-        # Inconsistencies between the number of boundaries and the number of dimensions
-        if len(aBoundarySet) != aNumberOfDimentions:
-            print("ERROR");
-        
+  
+    def set(self, aNumberOfDimentions, aBoundarySet, aCostFunction, aPSO):
+       
         # Store the boundary set
         # e.g. [[min_0, max_0][min_1, max_1][min_2, max_2]]
-        self.boundary_set = aBoundarySet;
+        self.boundary_set = copy.deepcopy(aBoundarySet);
         
         # Store the cost function
         self.cost_function = aCostFunction
-        
+
         # Store the PSO
         self.pso = aPSO;
-
-        print("aNumberOfDimentions", aNumberOfDimentions)
+        self.position = []
         # Initialise the particle's position and velocity
         for i in range(aNumberOfDimentions):
             # Get the boundaries
@@ -41,16 +38,23 @@ class Particle:
         
             # Compute the velocity
             self.velocity.append((random.uniform(min_i, max_i) - self.position[i]) / 2.0);
-        
+            
         # Compute the cost function
         self.computeCostFunction();
 
-    def getPosition(self):
-        return(self.position)
-    
+    def Particle(self, aParticle):
+        self.position = copy.deepcopy(aParticle.position)
+        self.velocity = copy.deepcopy(aParticle.velocity)
+        self.cost = aParticle.cost
+        self.best_known_position = copy.deepcopy(aParticle.best_known_position)
+        self.best_known_cost = aParticle.best_known_cost
+        self.boundary_set =copy.deepcopy(aParticle.aParticle.boundary_set)
+        self.pso = aParticle.pso
+        self.cost_function = aParticle.cost_function
+
+
     def computeCostFunction(self):
         # Compute the cost function
-        print(len(self.position), self.position)
         self.cost = self.cost_function(self.position)
         
         # Update the particle's best known position if needed
@@ -58,6 +62,7 @@ class Particle:
             self.best_known_cost = self.cost;
             self.best_known_position = self.position;
 
+        return self.cost;
     
     def update(self):
         self.updateVelocity();
@@ -65,24 +70,24 @@ class Particle:
     
     def updateVelocity(self):
     
-        w = 1 / (2 * log(2))
-        c = 1 / 2 + log(2)
+        w = 1 / (2 * math.log(2))
+        c = 1 / 2 + math.log(2)
         
-        for pos_i, part_best_pos_i, swarm_best_pos_i, vel_i in zip(self.position, self.best_known_position, self.pso.best_particle.position, self.velocity):
+        for pos_i, part_best_pos_i, swarm_best_pos_i, vel_i in zip(self.position, self.best_known_position, self.pso.best_particle_set[-1], self.velocity):
             vel_i = w * vel_i + random.uniform(0.0, c) * (part_best_pos_i - pos_i) + random.uniform(0.0, c) * (swarm_best_pos_i - pos_i)
 
     def updatePosition(self):
         # for each partical update the position
         for pos_i, vel_i in zip(self.position, self.velocity):
             pos_i += vel_i;
+
+        self.computeCostFunction();
         
     
-    def __repr__(self):
-        value = "Position:\t";
-        value += " ".join(str(i) for i in self.position);
-        value = "\tvelocity:\t";
-        value += " ".join(str(i) for i in self.velocity);
-        value = "\tcost:\t";
-        value += str(self.cost);
-        
-        return value;
+    def printing(self):
+        print("the position", self.position)
+        print()
+        print("the velocity", self.velocity)
+        print()
+        print("the cost", self.cost)
+        print("the boundary", self.boundary_set)
