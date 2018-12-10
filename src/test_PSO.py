b7f8a16a04cbf9f98bb9fc9494436b369bdaba79
Your Name <you@example.com>
2018-12-10 14:05:40 +0000

    Use 2D example.

--- src/test_PSO.py
+++ src/test_PSO.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+#import cProfile
+
 import math;
 
 import numpy as np
@@ -14,49 +16,132 @@ from matplotlib import animation
 from PSO import PSO
 import Particle
 
-g_number_of_dimensions = 3;
 g_number_of_particle   = 10;
-g_iteratio             = 100;
+g_iterations           = 100;
 
 
-def costFunction(aPosition):
 
-    #Compute the eqation (2x + y + 5z = 10)
-    sum = (2.0 * aPosition[0]) + aPosition[1] + (5.0 * aPosition[2])
 
-    return (abs(sum - 10))
-
-boundaries = []
-
+g_number_of_dimensions = 2;
+boundaries = [];
 for i in range(g_number_of_dimensions):
-    boundaries.append([0,10]);
+    boundaries.append([-5,5]);
 
-#my_particle = my_particle.Particle(g_number_of_dimensions, boundaries, costFunction, my_pso);
+def frange(start, stop, step):
+    i = start
+    while i < stop:
+         yield i
+         i += step
+
+def costFunction(aSolution):
+    sum = 0.0;
+    #
+    #for i in range(g_number_of_dimensions):
+    #    sum += aSolution[i] * aSolution[i];
+    #
+    sum += math.exp(-(math.pow(aSolution[0], 2) + math.pow(aSolution[1], 2)));
+    sum += 2.0 * math.exp(-(math.pow(aSolution[0]-1.7, 2) + math.pow(aSolution[1]-1.7, 2)));
+    sum *= -1.0;
+    #
+    return sum;
+
+
+def plot(anOptimiser):
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+
+    X = [];
+    Y = [];
+    Z = [];
+
+    linear_x = [];
+    linear_y = [];
+    linear_z = [];
+
+    for y in frange(boundaries[0][0], boundaries[0][1], 0.05):
+        #
+        Temp_X = [];
+        Temp_Y = [];
+        Temp_Z = [];
+        #
+        for x in frange(boundaries[1][0], boundaries[1][1], 0.05):
+            position = [x, y];
+            energy = costFunction(position);
+            Temp_X.append(x);
+            Temp_Y.append(y);
+            Temp_Z.append(energy);
+        #
+        X.append(Temp_X);
+        Y.append(Temp_Y);
+        Z.append(Temp_Z);
+
+    '''for best_solution, best_energy, current_solution, current_energy in zip(anOptimiser.best_solution_set, anOptimiser.best_energy_set, anOptimiser.current_solution_set, anOptimiser.current_energy_set):
+        linear_x.append(best_solution[0]);
+        linear_y.append(best_solution[1]);
+        linear_z.append(best_energy);
+
+    #    ax.scatter(   best_solution[0],    best_solution[1],    best_energy, c='r');
+    #    ax.scatter(current_solution[0], current_solution[1], current_energy, c='g');'''
+
+    # Plot a basic wireframe.
+    ax.plot_wireframe(np.array(X), np.array(Y), np.array(Z), rstride=10, cstride=10)
+
+
+
+
+
+    '''numframes = len(optimiser.best_solution_set)
+
+    xdata1, ydata1, zdata1 = [], [], [];
+    xdata2, ydata2, zdata2 = [], [], [];
+    xdata3, ydata3, zdata3 = [], [], [];
+
+    scat1 = ax.scatter([], [], [], marker='o', c='r', s=30) # Best solution
+    scat2 = ax.scatter([], [], [], marker='o', c='g', s=10) # All the current solutions
+    scat3 = ax.scatter([], [], [], marker='o', c='black', s=30) # The last current solution
+
+    positions = [];
+    def update(i):
+        global xdata1, ydata1, zdata1;
+        global xdata2, ydata2, zdata2;
+        global xdata3, ydata3, zdata3;
+
+        #positions.append([optimiser.best_solution_set[i][0], optimiser.best_solution_set[i][1], optimiser.best_energy_set[i]]);
+
+        xdata1, ydata1, zdata1 = [], [], [];
+        xdata3, ydata3, zdata3 = [], [], [];
+
+        if i == 0:
+            xdata2, ydata2, zdata2 = [], [], [];
+
+
+        #xdata1, ydata1, zdata1 = [], [], [];
+
+        # Best solution in red
+        xdata1.append(optimiser.best_solution_set[i][0]);
+        ydata1.append(optimiser.best_solution_set[i][1]);
+        zdata1.append(optimiser.best_energy_set[i]);
+
+        # All the current solution
+        xdata2.append(optimiser.current_solution_set[i][0]);
+        ydata2.append(optimiser.current_solution_set[i][1]);
+        zdata2.append(optimiser.current_energy_set[i]);
+
+        # The last current solution
+        xdata3.append(optimiser.current_solution_set[i][0]);
+        ydata3.append(optimiser.current_solution_set[i][1]);
+        zdata3.append(optimiser.current_energy_set[i]);
+
+        scat1._offsets3d = (xdata1, ydata1, zdata1)
+        scat2._offsets3d = (xdata2, ydata2, zdata2)
+        scat3._offsets3d = (xdata3, ydata3, zdata3)
+
+        return
+
+    ani = animation.FuncAnimation(fig, update, frames=range(numframes), repeat=False)'''
+    plt.show()
+
 
 my_pso = PSO(g_number_of_dimensions, boundaries, costFunction, g_number_of_particle);
-#my_pso = PSO.PSO();
-
-
-#print(my_particle);
-#print()
-print(my_pso)
-
-# Run the algorithm for 100 iteration
-for i in range(g_iteratio):
-    solution = my_pso.run();
-
-    position_x = [];
-    position_y = [];
-    position_z = [];
-
-    for particle in my_pso.particle_set:
-        position_x.append(particle.position[0]);
-        position_y.append(particle.position[1]);
-        position_z.append(particle.position[2]);
-
-    # Plot here the swarm
-
-
-    print(i, solution);
-#print(solution);
+plot(my_pso)
 print(my_pso)
