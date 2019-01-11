5044880c16531d816fe95922d192abac3e51d713
Your Name <you@example.com>
2019-01-11 14:16:45 +0000

    Factorisation of division.

--- src/lampProblem.py
+++ src/lampProblem.py
@@ -14,12 +14,12 @@ def getArea():
     return room_width * room_height;
 
 def areaEnlightened(aSetOfLamps):
-    
+
     global image_counter;
     global overlay_image;
-    
+
     number_of_lamps = int(len(aSetOfLamps) / 3);
-    
+
     # Create a black image (float, greyscale) of room_width x room_height pixels
     overlay_image = np.zeros((room_height, room_width, 1), np.float32)
 
@@ -28,7 +28,7 @@ def areaEnlightened(aSetOfLamps):
         x = int(aSetOfLamps[     i * 3 + 0])
         y = int(aSetOfLamps[     i * 3 + 1])
         on_off = aSetOfLamps[i * 3 + 2];
-        
+
         if on_off > 0.5:
             # Draw circles corresponding to the lamps
             black_image = np.zeros((room_height, room_width, 1), np.float32)
@@ -42,7 +42,7 @@ def areaEnlightened(aSetOfLamps):
         x = int(aSetOfLamps[     i * 3 + 0])
         y = int(aSetOfLamps[     i * 3 + 1])
         on_off = aSetOfLamps[i * 3 + 2];
-        
+
         if on_off > 0.5:
             # Plot the center of all the lamp (small radius) in black
             cv2.circle(temp_image, (x,y), 2, (0,0,0), -1)
@@ -51,7 +51,7 @@ def areaEnlightened(aSetOfLamps):
     filename = "lamp_" + str(image_counter) + ".png";
     image_counter += 1;
     #cv2.imwrite(filename, temp_image)
-    
+
     #cv2.imshow("Window", overlay_image)
     #cv2.imshow("Window1", temp_image)
 
@@ -64,30 +64,30 @@ def areaEnlightened(aSetOfLamps):
             if (overlay_image[i,j] > 0):
                 areaEnlight += min(overlay_image[i,j], 1)
 
-   
+
     #cv2.waitKey(1)
-    
+
     return areaEnlight;
 
 def areaOverlap(aSetOfLamps):
-    
+
     global overlay_image
-    
+
     areaOver = 0
     for i in range(room_width):
         for j in range(room_height):
-            
+
             if (overlay_image[i,j] > 1):
                 areaOver += overlay_image[i,j]
-    
+
     return areaOver;
 
 def fitnessFunction(aSetOfLamps, W=1):
     #print("areaEnlightened  ", areaEnlightened(aSetOfLamps))
     #print("areaOverlap  ", areaOverlap(aSetOfLamps))
     #print(aSetOfLamps)
-    
-    area_enlightened = areaEnlightened(aSetOfLamps) / getArea();
-    overlap          = areaOverlap(aSetOfLamps)     / getArea();
-    
-    return area_enlightened - W * overlap;
+
+    area_enlightened = areaEnlightened(aSetOfLamps);
+    overlap          = areaOverlap(aSetOfLamps);
+
+    return (area_enlightened - W * overlap) / getArea();
