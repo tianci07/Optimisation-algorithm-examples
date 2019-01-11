13a91dae912498815e7a0d6af3e8cc89555d0d7f
Your Name <you@example.com>
2019-01-11 15:31:24 +0000

    Add a global fitness and start working on the probabilities of operators.

--- src/EvolutionaryAlgorithm.py
+++ src/EvolutionaryAlgorithm.py
@@ -4,22 +4,44 @@ import Individual as IND
 
 class EvolutionaryAlgorithm:
 
-    def __init__(self, aNumberOfGenes, aBoundarySet, aFitnessFunction, aNumberOfIndividuals):
+    def __init__(self, aNumberOfGenes, aBoundarySet, aFitnessFunction, aNumberOfIndividuals, aGlobalFitnessFunction = 0):
+
+        # Probability of operators
+        sef.cross_over_probability  = 0.8;
+        sef.mutation_probability  = 0.0;
+        sef.new_blood_probability  = 0.2;
 
         # Store the population
         self.individual_set = [];
 
         # Keep track of the best individual
         self.individual_set.append(IND.Individual(aNumberOfGenes, aBoundarySet, aFitnessFunction));
-        best_individual_index = 0;
 
         # Create the population
         while (len(self.individual_set) < aNumberOfIndividuals):
-
             self.individual_set.append(IND.Individual(aNumberOfGenes, aBoundarySet, aFitnessFunction))
 
-            if (self.individual_set[best_individual_index].fitness < self.individual_set[-1].fitness):
-                best_individual_index = len(self.individual_set) - 1;
+        # Compute the global fitness
+        self.global_fitness = 0;
+        self.global_fitness_function = 0;
+        if aGlobalFitnessFunction:
+
+            set_of_individuals = [];
+            for ind in self.individual_set:
+                for gene in ind.genes:
+                    set_of_individuals.append(gene);
+
+            self.global_fitness_function = aGlobalFitnessFunction;
+            self.global_fitness = self.global_fitness_function(set_of_individuals);
+            print (self.global_fitness)
+
+        # Compute the fitness value of all the individual
+        # And keep track of who is the best individual
+        best_individual_index = 0;
+        for i in range(len(self.individual_set)):
+            self.individual_set[i].computeFitnessFunction();
+            if (self.individual_set[best_individual_index].fitness < self.individual_set[i].fitness):
+                best_individual_index = i;
 
         # Store the best individual
         self.best_individual = self.individual_set[best_individual_index].copy();
@@ -50,6 +72,12 @@ class EvolutionaryAlgorithm:
         # Genetic operatore
         while (len(offspring_population) < len(self.individual_set)):
 
+            # Draw a random number between 0 and 1
+
+
+
+
+
             # Select the parents from the population
             parent1_index = parent2_index = self.TournmentSelection()
 
@@ -61,8 +89,31 @@ class EvolutionaryAlgorithm:
 
             offspring_population[-1].gaussianMutation(aMutationRate);
 
-            if (offspring_population[best_individual_index].fitness < offspring_population[-1].fitness):
-                best_individual_index = len(offspring_population) - 1;
+
+
+
+
+
+
+
+
+        # Compute the global fitness
+        if self.global_fitness:
+
+            set_of_individuals = [];
+            for ind in self.individual_set:
+                for gene in ind.genes:
+                    set_of_individuals.append(gene);
+
+            self.global_fitness = self.global_fitness_function(set_of_individuals);
+
+        # Compute the fitness value of all the individual
+        # And keep track of who is the best individual
+        best_individual_index = 0;
+        for i in range(len(offspring_population)):
+            offspring_population[i].computeFitnessFunction();
+            if (offspring_population[best_individual_index].fitness < offspring_population[i].fitness):
+                best_individual_index = i;
 
         # Replace the parents by the offspring
         self.individual_set = offspring_population;
