abea20d03a264fb9084b5b3ef273d8117768d713
Your Name <you@example.com>
2019-03-28 10:22:57 +0000

    Clean up the code thanks to codacity.

--- src/test_SA.py
+++ src/test_SA.py
@@ -8,8 +8,8 @@ import matplotlib
 matplotlib.use("TkAgg")
 
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import animation
+from mpl_toolkits.mplot3d import Axes3D
 
 
 import SimulatedAnnealing as SA;
@@ -17,7 +17,13 @@ import SimulatedAnnealing as SA;
 
 
 
+xdata1, ydata1, zdata1 = [], [], [];
+xdata2, ydata2, zdata2 = [], [], [];
+xdata3, ydata3, zdata3 = [], [], [];
 
+scat1 = None;
+scat2 = None;
+scat3 = None;
 
 g_number_of_dimensions = 2;
 boundaries = [];
@@ -31,7 +37,7 @@ def frange(start, stop, step):
          i += step
 
 def costFunction(aSolution):
-    sum = 0.0;
+    cost = 0.0;
     #
     #for i in range(g_number_of_dimensions):
     #    sum += aSolution[i] * aSolution[i];
@@ -40,14 +46,53 @@ def costFunction(aSolution):
     #   -((exp(-(x^2 + y^2))) + 2 (exp(-((x-1.7)^2 + (y-1.7)^2))))
     # In LaTeX
     #
-    sum += math.exp(-(math.pow(aSolution[0], 2) + math.pow(aSolution[1], 2)));
-    sum += 2.0 * math.exp(-(math.pow(aSolution[0]-1.7, 2) + math.pow(aSolution[1]-1.7, 2)));
-    sum *= -1.0;
+    cost += math.exp(-(math.pow(aSolution[0], 2) + math.pow(aSolution[1], 2)));
+    cost += 2.0 * math.exp(-(math.pow(aSolution[0]-1.7, 2) + math.pow(aSolution[1]-1.7, 2)));
+    cost *= -1.0;
     #
-    return sum;
+    return cost;
 
+def update(i):
+
+    global xdata1, ydata1, zdata1;
+    global xdata2, ydata2, zdata2;
+    global xdata3, ydata3, zdata3;
+
+    global scat1, scat2, scat3;
+
+    xdata1, ydata1, zdata1 = [], [], [];
+    xdata3, ydata3, zdata3 = [], [], [];
+
+    if i == 0:
+        xdata2, ydata2, zdata2 = [], [], [];
+
+
+    #xdata1, ydata1, zdata1 = [], [], [];
+
+    # Best solution in red
+    xdata1.append(optimiser.best_solution_set[i][0]);
+    ydata1.append(optimiser.best_solution_set[i][1]);
+    zdata1.append(optimiser.best_energy_set[i]);
+
+    # All the current solution
+    xdata2.append(optimiser.current_solution_set[i][0]);
+    ydata2.append(optimiser.current_solution_set[i][1]);
+    zdata2.append(optimiser.current_energy_set[i]);
+
+    # The last current solution
+    xdata3.append(optimiser.current_solution_set[i][0]);
+    ydata3.append(optimiser.current_solution_set[i][1]);
+    zdata3.append(optimiser.current_energy_set[i]);
+
+    scat1._offsets3d = (xdata1, ydata1, zdata1)
+    scat2._offsets3d = (xdata2, ydata2, zdata2)
+    scat3._offsets3d = (xdata3, ydata3, zdata3)
+
+    return
 
 def plot(anOptimiser):
+    global scat1, scat2, scat3;
+
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
 
@@ -76,7 +121,7 @@ def plot(anOptimiser):
         Y.append(Temp_Y);
         Z.append(Temp_Z);
 
-    for best_solution, best_energy, current_solution, current_energy in zip(anOptimiser.best_solution_set, anOptimiser.best_energy_set, anOptimiser.current_solution_set, anOptimiser.current_energy_set):
+    for best_solution, best_energy, current_solution in zip(anOptimiser.best_solution_set, anOptimiser.best_energy_set, anOptimiser.current_solution_set):
         linear_x.append(best_solution[0]);
         linear_y.append(best_solution[1]);
         linear_z.append(best_energy);
@@ -93,63 +138,22 @@ def plot(anOptimiser):
 
     numframes = len(optimiser.best_solution_set)
 
-    xdata1, ydata1, zdata1 = [], [], [];
-    xdata2, ydata2, zdata2 = [], [], [];
-    xdata3, ydata3, zdata3 = [], [], [];
-
     scat1 = ax.scatter([], [], [], marker='o', c='r', s=30) # Best solution
     scat2 = ax.scatter([], [], [], marker='o', c='g', s=10) # All the current solutions
     scat3 = ax.scatter([], [], [], marker='o', c='black', s=30) # The last current solution
 
-    positions = [];
-    def update(i):
-        global xdata1, ydata1, zdata1;
-        global xdata2, ydata2, zdata2;
-        global xdata3, ydata3, zdata3;
-
-        #positions.append([optimiser.best_solution_set[i][0], optimiser.best_solution_set[i][1], optimiser.best_energy_set[i]]);
-
-        xdata1, ydata1, zdata1 = [], [], [];
-        xdata3, ydata3, zdata3 = [], [], [];
-
-        if i == 0:
-            xdata2, ydata2, zdata2 = [], [], [];
-
-
-        #xdata1, ydata1, zdata1 = [], [], [];
-
-        # Best solution in red
-        xdata1.append(optimiser.best_solution_set[i][0]);
-        ydata1.append(optimiser.best_solution_set[i][1]);
-        zdata1.append(optimiser.best_energy_set[i]);
-
-        # All the current solution
-        xdata2.append(optimiser.current_solution_set[i][0]);
-        ydata2.append(optimiser.current_solution_set[i][1]);
-        zdata2.append(optimiser.current_energy_set[i]);
-
-        # The last current solution
-        xdata3.append(optimiser.current_solution_set[i][0]);
-        ydata3.append(optimiser.current_solution_set[i][1]);
-        zdata3.append(optimiser.current_energy_set[i]);
-
-        scat1._offsets3d = (xdata1, ydata1, zdata1)
-        scat2._offsets3d = (xdata2, ydata2, zdata2)
-        scat3._offsets3d = (xdata3, ydata3, zdata3)
-
-        return
 
     ani = animation.FuncAnimation(fig, update, frames=range(numframes), repeat=False)
 
     # Set up formatting for the movie files
-    '''Writer = animation.writers['ffmpeg']
-    writer = Writer(fps=15, metadata=dict(artist='Me'), bitrate=1800)
-    ani.save('test_SA.mp4', writer=writer)'''
+    #Writer = animation.writers['ffmpeg']
+    #writer = Writer(fps=15, metadata=dict(artist='Me'), bitrate=1800)
+    #ani.save('test_SA.mp4', writer=writer)
 
     # Set up formatting for the movie files
-    '''Writer = animation.writers['imagemagick']
+    Writer = animation.writers['imagemagick']
     writer = Writer()
-    ani.save('test_SA.gif', writer=writer)'''
+    ani.save('test_SA.gif', writer=writer)
 
     plt.show()
 
