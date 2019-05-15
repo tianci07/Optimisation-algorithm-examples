0a6041524b49bd275f8c30fb6644b78582dc6b13
Your Name <you@example.com>
2019-05-15 13:00:42 +0100

    Simplify the code.

--- src/test_EA.py
+++ src/test_EA.py
@@ -3,9 +3,6 @@
 #import cProfile
 
 import math;
-import traceback
-
-import numpy as np
 
 import matplotlib
 matplotlib.use("TkAgg")
@@ -14,19 +11,18 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import animation
 
-import EvolutionaryAlgorithm  as EA
+from EvolutionaryAlgorithm import *
 
+# Selection operators
 from TournamentSelection      import *
 from RouletteWheel            import *
 from RankSelection            import *
 
+# Genetic operators
 from ElitismOperator          import *
 from BlendCrossoverOperator   import *
 from GaussianMutationOperator import *
 from NewBloodOperator         import *
-
-
-import Individual
 
 g_number_of_individuals = 10;
 g_iterations            = 20;
@@ -35,9 +31,11 @@ g_number_of_genes       = 2;
 g_max_mutation_sigma = 0.5;
 g_min_mutation_sigma = 0.01;
 
-boundaries = [];
+g_current_sigma = g_max_mutation_sigma;
+
+g_boundaries = [];
 for i in range(g_number_of_genes):
-    boundaries.append([-5,5]);
+    g_boundaries.append([-5,5]);
 
 def fitnessFunction(aSolution):
     fitness = 0.0;
@@ -51,21 +49,11 @@ def fitnessFunction(aSolution):
     #
     return -fitness;
 
-optimiser = EA.EvolutionaryAlgorithm(g_number_of_genes, boundaries, fitnessFunction, g_number_of_individuals)
-#optimiser.setSelectionOperator(TournamentSelection(2));
-#optimiser.setSelectionOperator(RouletteWheel());
-optimiser.setSelectionOperator(RankSelection());
-print(optimiser.selection_operator)
-
-elitism = ElitismOperator(0.1);
-new_blood = NewBloodOperator(0.3);
-gaussian_mutation = GaussianMutationOperator(0.1, 0.4);
-blend_cross_over = BlendCrossoverOperator(0.5, gaussian_mutation);
-
-optimiser.addGeneticOperator(new_blood);
-optimiser.addGeneticOperator(gaussian_mutation);
-optimiser.addGeneticOperator(blend_cross_over);
-optimiser.addGeneticOperator(elitism);
+# Print the current state in the console
+def printCurrentStates(anIteration, anOptimiser):
+    print("Iteration:\t", anIteration);
+    print(optimiser);
+    print();
 
 def frange(start, stop, step):
     i = start
@@ -73,31 +61,28 @@ def frange(start, stop, step):
          yield i
          i += step
 
+def createFigure():
+    # Create the figure and axes
+    fig = plt.figure();
+    ax = fig.add_subplot(111, projection='3d');
 
-def plot(anOptimiser):
-    global best_individual_x;
-    global best_individual_y;
-    global best_individual_z;
-
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
-
+    # Create the wireframe
     X = [];
     Y = [];
     Z = [];
 
-    for y in frange(boundaries[0][0], boundaries[0][1], 0.05):
+    for y in frange(g_boundaries[0][0], g_boundaries[0][1], 0.05):
         #
         Temp_X = [];
         Temp_Y = [];
         Temp_Z = [];
         #
-        for x in frange(boundaries[1][0], boundaries[1][1], 0.05):
+        for x in frange(g_boundaries[1][0], g_boundaries[1][1], 0.05):
             genes = [x, y];
-            energy = fitnessFunction(genes);
+            fitness = fitnessFunction(genes);
             Temp_X.append(x);
             Temp_Y.append(y);
-            Temp_Z.append(-energy);
+            Temp_Z.append(-fitness);
         #
         X.append(Temp_X);
         Y.append(Temp_Y);
@@ -106,83 +91,77 @@ def plot(anOptimiser):
     # Plot a basic wireframe.
     ax.plot_wireframe(np.array(X), np.array(Y), np.array(Z), rstride=10, cstride=10)
 
-    numframes = len(best_individual_x)
+    # Plot the current best
+    scat1 = ax.scatter([], [], [], marker='o', c='r', s=30)
 
-    scat1 = ax.scatter([], [], [], marker='o', c='r', s=30) # Best solution
-    scat2 = ax.scatter([], [], [], marker='o', c='g', s=10) # All the current solutions
+    # Plot the current population
+    scat2 = ax.scatter([], [], [], marker='o', c='g', s=10)
 
+    return fig, ax, scat1, scat2;
 
-    def update(i):
-        global best_individual_x;
-        global best_individual_y;
-        global best_individual_z;
+def update(i):
+    global g_current_sigma;
+    global optimiser;
+    global gaussian_mutation;
+    global scat1;
 
-        global set_individual_set_x;
-        global set_individual_set_y;
-        global set_individual_set_z;
+    # Print the current state in the console
+    printCurrentStates(i, optimiser);
 
-        xdata1, ydata1, zdata1 = [], [], [];
+    # This is not the initial population
+    if i != 0:
+        # Make sure the mutation variance is up-to-date
+        gaussian_mutation.setMutationVariance(g_current_sigma);
 
-        # Best solution in red
-        xdata1.append(best_individual_x[i]);
-        ydata1.append(best_individual_y[i]);
-        zdata1.append(best_individual_z[i]);
-
-        # All the current solution
-        xdata2 = set_individual_set_x[i];
-        ydata2 = set_individual_set_y[i];
-        zdata2 = set_individual_set_z[i];
-
-        scat1._offsets3d = (xdata1, ydata1, zdata1)
-        scat2._offsets3d = (xdata2, ydata2, zdata2)
-
-        return
-
-    ani = animation.FuncAnimation(fig, update, frames=range(numframes), repeat=False)
-    plt.show()
-
-
-try:
-    best_individual_x = [];
-    best_individual_y = [];
-    best_individual_z = [];
-
-    set_individual_set_x = [];
-    set_individual_set_y = [];
-    set_individual_set_z = [];
-
-    for i in range(g_iterations):
-        sigma = g_min_mutation_sigma + (g_iterations - 1 - i) / (g_iterations - 1) * (g_max_mutation_sigma - g_min_mutation_sigma);
-        gaussian_mutation.setMutationVariance(sigma);
+        # Run the optimisation loop
         optimiser.run();
 
-        # Store the best individual
-        best_individual_x.append(optimiser.best_individual.genes[0])
-        best_individual_y.append(optimiser.best_individual.genes[1])
-        best_individual_z.append(-optimiser.best_individual.fitness)
+        # Print the current state in the console
+        printCurrentStates(i, optimiser);
 
-        # Store the current swarm
-        individual_set_x = [];
-        individual_set_y = [];
-        individual_set_z = [];
+        # Update the mutation variance so that it varies linearly from g_max_mutation_sigma to
+        # g_min_mutation_sigma
+        g_current_sigma -= (g_max_mutation_sigma - g_min_mutation_sigma) / (g_iterations - 1);
 
-        for individual in optimiser.individual_set:
-            individual_set_x.append(individual.genes[0])
-            individual_set_y.append(individual.genes[1])
-            individual_set_z.append(-individual.fitness)
+    # Best solution in red
+    xdata1, ydata1, zdata1 = [], [], [];
+    xdata1.append(optimiser.best_individual.genes[0]);
+    ydata1.append(optimiser.best_individual.genes[1]);
+    zdata1.append(-optimiser.best_individual.fitness);
+    scat1._offsets3d = (xdata1, ydata1, zdata1)
 
-        set_individual_set_x.append(individual_set_x);
-        set_individual_set_y.append(individual_set_y);
-        set_individual_set_z.append(individual_set_z);
+    # All the current solution
+    xdata2, ydata2, zdata2 = [], [], [];
+    for individual in optimiser.individual_set:
+        xdata2.append(individual.genes[0]);
+        ydata2.append(individual.genes[1]);
+        zdata2.append(-individual.fitness);
+    scat2._offsets3d = (xdata2, ydata2, zdata2)
 
-    print(optimiser)
-    print(optimiser.best_individual)
-    print(elitism)
-    print(new_blood);
-    print(gaussian_mutation);
-    print(blend_cross_over);
+# Create an EA
+optimiser = EvolutionaryAlgorithm(g_number_of_genes, g_boundaries, fitnessFunction, g_number_of_individuals)
 
-    plot(optimiser)
+# Set the selection operator
+#optimiser.setSelectionOperator(TournamentSelection(2));
+#optimiser.setSelectionOperator(RouletteWheel());
+optimiser.setSelectionOperator(RankSelection());
 
-except:
-    traceback.print_exc()
+# Create the genetic operators
+elitism = ElitismOperator(0.1);
+new_blood = NewBloodOperator(0.3);
+gaussian_mutation = GaussianMutationOperator(0.1, 0.4);
+blend_cross_over = BlendCrossoverOperator(0.5, gaussian_mutation);
+
+# Add the genetic operators to the EA
+optimiser.addGeneticOperator(new_blood);
+optimiser.addGeneticOperator(gaussian_mutation);
+optimiser.addGeneticOperator(blend_cross_over);
+optimiser.addGeneticOperator(elitism);
+
+# Create a figure (Matplotlib)
+fig, ax, scat1, scat2 = createFigure();
+
+# Run the visualisation
+numframes = g_iterations + 1;
+ani = animation.FuncAnimation(fig, update, frames=range(numframes), repeat=False)
+plt.show()
