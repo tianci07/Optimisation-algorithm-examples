edaac94b64080297ad6ccb3d2ff6ef956249ac44
Your Name <you@example.com>
2019-05-16 12:08:29 +0100

    Move the objective function out of the class (to remove comment on Codacy).

--- src/AckleyFunction.py
+++ src/AckleyFunction.py
@@ -4,6 +4,19 @@ import math
 
 from ObjectiveFunction import *
 
+def objectiveFunction(aSolution):
+
+    # Function:
+    #   f(x,y)=-20&amp;\exp \left[-0.2{\sqrt {0.5\left(x^{2}+y^{2}\right)}}\right]\\&amp;{}-\exp \left[0.5\left(\cos 2\pi x+\cos 2\pi y\right)\right]+e+20}}
+    # In LaTeX
+
+    cost = 0.0;
+    cost += -20.0 * math.exp(0.2 * -(math.sqrt(math.pow(aSolution[0], 2) + math.pow(aSolution[1], 2))));
+    cost += - math.exp(0.5 * (math.cos(2.0 * math.pi * aSolution[0]) + math.cos(2.0 * math.pi * aSolution[1])));
+    cost += math.e + 20.0;
+
+    return cost;
+
 class AckleyFunction(ObjectiveFunction):
     def __init__(self):
 
@@ -15,20 +28,7 @@ class AckleyFunction(ObjectiveFunction):
 
         super().__init__(number_of_dimensions,
                          boundaries,
-                         self.objectiveFunction,
+                         objectiveFunction,
                          1);
 
         self.global_optimum = [0, 0];
-
-    def objectiveFunction(self, aSolution):
-
-        # Function:
-        #   f(x,y)=-20&amp;\exp \left[-0.2{\sqrt {0.5\left(x^{2}+y^{2}\right)}}\right]\\&amp;{}-\exp \left[0.5\left(\cos 2\pi x+\cos 2\pi y\right)\right]+e+20}}
-        # In LaTeX
-
-        cost = 0.0;
-        cost += -20.0 * math.exp(0.2 * -(math.sqrt(math.pow(aSolution[0], 2) + math.pow(aSolution[1], 2))));
-        cost += - math.exp(0.5 * (math.cos(2.0 * math.pi * aSolution[0]) + math.cos(2.0 * math.pi * aSolution[1])));
-        cost += math.e + 20.0;
-
-        return cost;
