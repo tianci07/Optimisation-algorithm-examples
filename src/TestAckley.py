7657ffb5c753c6b67efde14fc66574c7d3f79b17
Your Name <you@example.com>
2019-05-16 10:44:26 +0100

    Print some results in the terminal.

--- src/TestAckley.py
+++ src/TestAckley.py
@@ -41,7 +41,6 @@ def visualisationCallback():
 
     # Make sure the mutation variance is up-to-date
     gaussian_mutation.setMutationVariance(g_current_sigma);
-    print(g_current_sigma)
 
 
 
@@ -55,9 +54,9 @@ optimiser.setSelectionOperator(RankSelection());
 
 # Create the genetic operators
 elitism = ElitismOperator(0.1);
-new_blood = NewBloodOperator(0.3);
-gaussian_mutation = GaussianMutationOperator(0.1, 0.4);
-blend_cross_over = BlendCrossoverOperator(0.5, gaussian_mutation);
+new_blood = NewBloodOperator(0.1);
+gaussian_mutation = GaussianMutationOperator(0.1, 0.2);
+blend_cross_over = BlendCrossoverOperator(0.6, gaussian_mutation);
 
 # Add the genetic operators to the EA
 optimiser.addGeneticOperator(new_blood);
@@ -67,20 +66,30 @@ optimiser.addGeneticOperator(elitism);
 
 test_problem.number_of_evaluation = 0;
 optimiser.plotAnimation(g_iterations, visualisationCallback);
-print("EA.number_of_evaluation:\t", test_problem.number_of_evaluation)
-print("EA solution:\t", optimiser.best_solution);
+EA_number_of_evaluation = test_problem.number_of_evaluation
+EA_solution = optimiser.best_solution;
 
 # Optimisation and visualisation
 test_problem.number_of_evaluation = 0;
 optimiser = PSO(test_problem, g_number_of_individuals);
 optimiser.plotAnimation(g_iterations);
-print("PSO.number_of_evaluation:\t", test_problem.number_of_evaluation)
-print("PSO solution:\t", optimiser.best_solution);
+PSO_number_of_evaluation = test_problem.number_of_evaluation
+PSO_solution = optimiser.best_solution;
 
 
 # Optimisation and visualisation
 test_problem.number_of_evaluation = 0;
 optimiser = SimulatedAnnealing(test_problem, 5000, 0.04);
 optimiser.plotAnimation(211);
-print("SA.number_of_evaluation:\t", test_problem.number_of_evaluation)
-print("SA solution:\t", optimiser.best_solution);
+SA_number_of_evaluation = test_problem.number_of_evaluation
+SA_solution = optimiser.best_solution;
+
+print("EA.number_of_evaluation:\t", EA_number_of_evaluation)
+print("EA solution:\t", EA_solution);
+print()
+print("PSO.number_of_evaluation:\t", PSO_number_of_evaluation)
+print("PSO solution:\t", PSO_solution);
+print()
+print("SA.number_of_evaluation:\t", SA_number_of_evaluation)
+print("SA solution:\t", SA_solution);
+print()
