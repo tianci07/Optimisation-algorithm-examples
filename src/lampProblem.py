b3e663ab7d0a5afd85cb707f682b6eb35a7f2390
Your Name <you@example.com>
2019-05-17 12:56:30 +0100

    Remove duplicated code.

--- src/lampProblem.py
+++ src/lampProblem.py
@@ -1,6 +1,5 @@
 import numpy as np
 import cv2
-import copy
 
 from ObjectiveFunction import *
 
@@ -55,17 +54,13 @@ class LampProblem(ObjectiveFunction):
         overlap          = areaOverlap(overlay_image);
         fitness = (area_enlightened - self.W * overlap) / self.getArea();
 
-        if self.verbose:
-            print(fitness, area_enlightened, overlap);
-
+        if self.verbose >= 1:
             temp_image = np.copy(overlay_image);
 
             for i in range(self.number_of_lamps):
-                x = int(aSetOfLamps[ i * 3 + 0])
-                y = int(aSetOfLamps[ i * 3 + 1])
-                on_off = aSetOfLamps[i * 3 + 2];
+                x, y, on_off = self.getLampDetails(aSetOfLamps, i)
 
-                if on_off > 0.5:
+                if on_off:
                     # Plot the center of all the lamp (small radius) in black
                     cv2.circle(temp_image, (x,y), 2, (0,0,0), -1)
 
@@ -75,16 +70,6 @@ class LampProblem(ObjectiveFunction):
                 self.global_fitness = fitness;
                 self.vis_image[0:self.room_height,10+self.room_width:10+2*self.room_width] = temp_image / np.max(temp_image);
 
-            # Save the image (use image_counter in the file name)
-            filename = "lamp_" + str(self.image_counter) + ".png";
-            self.image_counter += 1;
-            temp_image -= np.min(temp_image);
-            temp_image /= np.max(temp_image);
-            temp_image *= 255;
-            cv2.imwrite(filename, temp_image);
-
-
-
             cv2.imshow('vis', self.vis_image);
 
             new_window_name = "fitness (" + str(fitness) + "), " + "area_enlightened (" + str(area_enlightened) + "), " + "overlap (" + str(overlap) + ")";
@@ -93,6 +78,18 @@ class LampProblem(ObjectiveFunction):
 
             cv2.waitKey(1);
 
+        if self.verbose >= 2:
+
+            # Save the image (use image_counter in the file name)
+            filename = "lamp_" + str(self.image_counter) + ".png";
+            self.image_counter += 1;
+            temp_image -= np.min(temp_image);
+            temp_image /= np.max(temp_image);
+            temp_image *= 255;
+            cv2.imwrite(filename, temp_image);
+
+        if self.verbose >= 3:
+            print(fitness, area_enlightened, overlap);
 
         return fitness;
 
@@ -103,6 +100,12 @@ class LampProblem(ObjectiveFunction):
         cv2.circle(black_image, (x,y), self.lamp_radius, (l, l, l), -1)
         np.add(overlay_image, black_image, overlay_image);
 
+    def getLampDetails(self, aSetOfLamps, i):
+        x = int(aSetOfLamps[ i * 3 + 0])
+        y = int(aSetOfLamps[ i * 3 + 1])
+        on_off = (aSetOfLamps[i * 3 + 2] > 0.5);
+        return x, y, on_off;
+
     def createLampMap(self, aSetOfLamps):
 
         # Create a black image (float32, greyscale) of room_width x room_height pixels
@@ -110,11 +113,9 @@ class LampProblem(ObjectiveFunction):
 
         # print the position of all the lamps
         for i in range(self.number_of_lamps):
-            x = int(aSetOfLamps[ i * 3 + 0])
-            y = int(aSetOfLamps[ i * 3 + 1])
-            on_off = aSetOfLamps[i * 3 + 2];
+            x, y, on_off = self.getLampDetails(aSetOfLamps, i)
 
-            if on_off > 0.5:
+            if on_off:
                 # Draw circles corresponding to the lamps
                 self.addLampToImage(overlay_image, x, y, 1);
 
